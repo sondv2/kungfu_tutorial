fe9d7e3d17bd9fc71d6e437cf8401a9fd876187f
Pranoy Radhakrishnan <pranoyalkr@gmail.com>
2018-02-04 14:57:50 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,44 +4,40 @@ from scipy import  misc
 from keras.models import model_from_json
 import pickle
 
-
-
 classifier_f = open("int_to_word_out.pickle", "rb")
 int_to_word_out = pickle.load(classifier_f)
 classifier_f.close()
 
+def load_model():
 
+    # load json and create model
+    json_file = open('model_face.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model_face.h5")
+    print("Loaded model from disk")
+    return loaded_model
 
-# load json and create model
-json_file = open('model_face.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model_face.h5")
-print("Loaded model from disk")
+def pre_process(image):
+    image = image.astype('float32')
+    image = image / 255.0
+    return image
 
+def load_image():
 
-img=os.listdir("predict")[0]
-image=np.array(misc.imread("predict/"+img))
-image = misc.imresize(image, (64, 64))
-image=np.array([image])
-image = image.astype('float32')
-image = image / 255.0
+    img=os.listdir("predict")[0]
+    image=np.array(misc.imread("predict/"+img))
+    image = misc.imresize(image, (64, 64))
+    image=np.array([image])
+    image=pre_process(image)
+    return image
 
-prediction=loaded_model.predict(image)
+image=load_image()
+model=load_model()
+prediction=model.predict(image)
 
 print(prediction)
-
 print(np.max(prediction))
-
 print(int_to_word_out[np.argmax(prediction)])
-
-
-
-
-
-
-
-
-
