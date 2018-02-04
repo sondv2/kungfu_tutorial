4b528c42dafed52b0cf79c6c3ccf81fad7fda556
Pranoy Radhakrishnan <pranoyalkr@gmail.com>
2018-02-04 14:47:29 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,60 +1,77 @@
-
 import numpy
-
 import matplotlib.pyplot as plt
-
 from keras.layers import Dropout
 from keras.layers import Flatten
 from keras.constraints import maxnorm
 from keras.optimizers import SGD
 from keras.layers import Conv2D
-
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
 import load_data
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.model_selection import train_test_split
 
-import keras
 K.set_image_dim_ordering('tf')
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
+
+def pre_process(X):
+
+    # normalize inputs from 0-255 to 0.0-1.0
+    X=X.astype('float32')
+    X = X / 255.0
+    return X
+
+def one_hot_encode(y):
+
+    # one hot encode outputs
+    y = np_utils.to_categorical(y)
+    num_classes = y.shape[1]
+    return y,num_classes
+
+def define_model(num_classes,epochs):
+    # Create the model
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    # Compile model
+    lrate = 0.01
+    decay = lrate/epochs
+    sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    print(model.summary())
+    return model
+
+
 # load data
-(X_train,X_test,y_train,y_test)=load_data.data_set
+X,y=load_data.load_datasets()
+
+# pre process
+X=pre_process(X)
+
+#one hot encode
+y,num_classes=one_hot_encode(y)
 
 
-# normalize inputs from 0-255 to 0.0-1.0
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train = X_train / 255.0
-X_test = X_test / 255.0
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_train.shape[1]
-# Create the model
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
-model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
-# Compile model
+#split dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=7)
+
 epochs = 10
-lrate = 0.01
-decay = lrate/epochs
-sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-print(model.summary())
-#callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto')]
-callbacks=[keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)]
+#define model
+model=define_model(num_classes,epochs)
+
+
 # Fit the model
-history=model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32,shuffle=True,callbacks=callbacks)
+history=model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
 
 # list all data in history
 print(history.history.keys())
@@ -89,19 +106,3 @@ with open("model_face.json", "w") as json_file:
 # serialize weights to HDF5
 model.save_weights("model_face.h5")
 print("Saved model to disk")
-
-
-
-
-# later...
-
-# load json and create model
-#json_file = open('model_face.json', 'r')
-#loaded_model_json = json_file.read()
-#json_file.close()
-#loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-#loaded_model.load_weights("model_face.h5")
-#print("Loaded model from disk")
-
-
